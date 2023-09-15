d70dbc4bf384a370a5f487af561f2bbe6e9cd1af
Rodrigo322 <rodrigollrdglucas@gmail.com>
2023-09-15 15:15:09 -0300

    update

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,22 @@
-import { useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   Alert,
+  FlatList,
+  RefreshControl,
   SafeAreaView,
   ScrollView,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { Card, CardProps } from "../../components/CardProduct";
 import { Header } from "../../components/Header";
 import { Highlights } from "../../components/Highlights";
-
 import { api } from "../../services/api";
-
 import { useTabContext } from "../../contexts/TabContext";
-
 import { checkInternetConnection } from "../../utils/netInfo";
 import { styles } from "./styles";
 
@@ -25,31 +24,38 @@ export function Home() {
   const [products, setProducts] = useState<CardProps[]>([]);
   const { idBank, setShowTab } = useTabContext();
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
   const { navigate } = useNavigation();
 
-  useEffect(() => {
+  async function fetchProducts() {
     setLoading(true);
-    if (idBank === "") {
-      Alert.alert("Anteção", "Por favor selecione uma banca!");
+    if (!idBank) {
+      Alert.alert("Atenção", "Por favor, selecione uma banca!");
       setLoading(false);
-      return navigate("SelectBank");
-    } else {
-      setProducts([]);
-      checkInternetConnection().then((isConnected) => {
-        if (!isConnected) {
-          Alert.alert("Sem conexão", "Você está sem conexão com a internet.");
-          return;
-        }
-        api
-          .get<CardProps[]>(`/get-all-product/store/${idBank}`)
-          .then((response) => {
-            setProducts(response.data);
-            setLoading(false);
-          });
-      });
+      navigate("SelectBank");
+      return;
     }
-  }, [idBank]);
+
+    checkInternetConnection().then((isConnected) => {
+      if (!isConnected) {
+        Alert.alert("Sem conexão", "Você está sem conexão com a internet.");
+        setLoading(false);
+        return;
+      }
+
+      api
+        .get<CardProps[]>(`/get-all-product/store/${idBank}`)
+        .then(({ data }) => {
+          setProducts(data);
+          setLoading(false);
+        });
+    });
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [idBank, refreshing]);
 
   function handleSelectBank() {
     setShowTab(false);
@@ -59,33 +65,40 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Header />
-      <ScrollView
-        nestedScrollEnabled={true}
-        style={styles.scrollHome}
-        scrollEventThrottle={16}
-      >
-        <SafeAreaView style={{ flex: 1, paddingTop: 20 }}>
-          <Highlights />
-          <View style={styles.Header}>
-            <Text style={styles.infoHomeText}>Frutas, Verduras</Text>
-            <TouchableOpacity onPress={handleSelectBank}>
-              <Text style={styles.HeaderText}>Selecionar banca</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.listHomeProduct}>
-            {loading && <ActivityIndicator size="large" color="#019972" />}
-            {products.map((product) => (
+
+      <SafeAreaView style={{ flex: 1, paddingTop: 20 }}>
+        <Highlights />
+        <View style={styles.Header}>
+          <Text style={styles.infoHomeText}>Frutas, Verduras</Text>
+          <TouchableOpacity onPress={handleSelectBank}>
+            <Text style={styles.HeaderText}>Selecionar banca</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.listHomeProduct}>
+          <FlatList
+            numColumns={2}
+            data={products}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
               <Card
-                key={product.id}
-                image={product.image}
-                name={product.name}
-                id={product.id}
-                price={product.price}
+                key={item.id}
+                image={item.image}
+                name={item.name}
+                id={item.id}
+                price={item.price}
               />
-            ))}
-          </View>
-        </SafeAreaView>
-      </ScrollView>
+            )}
+            ListEmptyComponent={() => <Text>Esta banca está sem produtos</Text>}
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={fetchProducts} // A função de atualização
+                colors={["#019972"]} // Cor do indicador de carregamento
+              />
+            }
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
